46bac99e3720ef5c2dc0d28e49dd2b0711a606df
lobarseniy <lob.arseniy@ylab.io>
2020-04-13 21:54:37 +0500

    finish 360 image viewer

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require('electron');
 
 app.on('ready', function(){
 
-    var win = new BrowserWindow({ width: 1280, height: 720 });
+    let  win = new BrowserWindow({ width: 1280, height: 720 });
 
     win.loadFile('index.html')
 
